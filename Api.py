87213b09ea3a5bc03e57d4d17a60342719237d58
CloudTechnology1 <e.ibok@cloud-tech.africa>
2024-08-15 11:44:58 +0100

    new push

---
+++ Api.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/time', methods=['GET'])
+def get_time():
+    now = datetime.utcnow().isoformat()
+    return jsonify({"current_time": now})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
---
+++ my-tf-script/version.tf
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.47.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "google" {
+    credentials = file("/home/runner/work/_temp/105bf773-f0e2-4d81-a205-59dde38e65c8/terraform-bin plan -input=false -var-file=terraform.tfvars")
+    project     = var.project
+    region      = var.region 
+}
